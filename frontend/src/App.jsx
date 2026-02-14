d4e7775bfddd6317fc1b36847a37d58318378de7
Sanjana0630 <sanjanamoralwar17@gmail.com>
2026-02-14 19:37:16 +0530

    Fixed router structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import DashboardLayout from './components/DashboardLayout';
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
