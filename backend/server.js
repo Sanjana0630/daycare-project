c877f1015cfdb1d11a289ff2c0cc5dcb52525e03
Sanjana0630 <sanjanamoralwar17@gmail.com>
2026-04-04 13:06:32 +0530

    fixed error while fetching review

--- backend/server.js
+++ backend/server.js
@@ -48,7 +48,7 @@ app.use("/api/feedback", feedbackRoutes);
 // Database Connection
 connectDB();
 
-const PORT = 5007;
+const PORT = process.env.PORT || 5005;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
